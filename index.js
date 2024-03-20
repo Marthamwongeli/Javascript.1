84b313f653b9a6a9596a6e38520475617ca15b7d
Marthamwongeli <marthamutuaa@gmail.com>
2024-03-20 21:55:44 +0300

    added files

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // Extract the last four characters from the string below;"extravaganza"
 let a="extravaganza";
-console.log(a[8],a[9],a[10],a[11]);
+console.log(a.slice(8,));
 
-// Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
+// // Insert the following string at the fourth index of the below variable:"eat"const food = "The quick fox jumped over the lazy dog"
 
 const food="The quick fox jumped over the lazy dog";
-let changed= food.slice(0,4) + " eat " + food.slice(4);
-console.log({changed});
+let newFood='eat';
+let position= 4;
+let newString= food.slice(0,4) + 'eat' + food.slice(4);
+console.log(newString);
 
-// Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
+// // Count how many times the following string appears in the string variable:1. "the"2."brown"const story= "The quick brown fox jumps over the lazy dog"
 const story="The quick brown fox jumps over the lazy dog";
-const count1=(count.match(/the/gi)||[].length);
-const count2=(count.match(/brown/gi)|| [].length);
-
-console.log("Number of occurrences of'the':",count1);
-console.log("Number of occurrences of'brown':",count2);
+const count = (story.match(/the/g) || []).length;
+console.log(count)
+const count2 = (story.match(/brown/g) || []).length;
+console.log(count2);
+    
 
 // Using JavaScript, find the following words from the following strings:1. "are"const string1 = "The pupils are reading in the library";2. "sitting"const string2 = "The child was sitting on the table before it fell"
 
 const string1="The pupils are reading in the library";
-console.log({item:string1.indexOf('are')});
+let p ="are";
+console.log(string1.includes(p));
 
 const string2="The child was sitting on the table before it fell";
-console.log({item:string2.indexOf('sitting')});
+let r ='sitting';
+console.log(string2.includes(r));
 
 
 
@@ -36,8 +40,17 @@ console.log(k.toLowerCase());
 let x="undERneath";
 console.log(x.toLowerCase());
 
-let z="A wonderful world";
-console.log(z.toTitlecase());
+function titleCase(str) {
+	return str.toLowerCase().split(' ').map(function (word) {
+		return (word.charAt(0).toUpperCase() + word.slice(1));
+	}).join(' ');
+}
+console.log(titleCase("A wonderful world"));
+
+
+
+
+
 
 
 
